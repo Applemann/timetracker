c1b930430ac5a6ad4dc25ed4ed5c24788addef4f
Martin Jablečník <martin.jablecnik@email.cz>
2016-03-07 16:41:27 +0100

    changed tracking file to json file

--- track.py
+++ track.py
@@ -3,31 +3,63 @@
 from datetime import datetime
 import sys, re
 import os.path
+import json
+import collections
 
 
 #CONFIG_PATH='/home/martin/.timetrack'
 CONFIG_PATH='config'
 
 TIME_TRACK_FILE='%s/martin_%s.tm' % (CONFIG_PATH, datetime.now().strftime("%Y-%m"))
-TIME_TRACK_JSON='%s/martin_%s.json' % (CONFIG_PATH, datetime.now().strftime("%Y-%m"))
-#TIME_TRACK_FILE=TIME_TRACK_JSON
+#TIME_TRACK_JSON='%s/martin_%s.json' % (CONFIG_PATH, datetime.now().strftime("%Y-%m"))
+TIME_TRACK_JSON='%s/timetrack.json' % CONFIG_PATH
+TIME_TRACK_FILE=TIME_TRACK_JSON
 TIME_TRACK_STATUS='%s/status' % CONFIG_PATH
 
 
+def get_now_date():
+    return datetime.now().strftime("%Y-%m-%d")
+
+def get_now_time():
+    return datetime.now().strftime("%H:%M")
 
 
-### SHOW TIMES
-def show_times():
-    file = open(TIME_TRACK_FILE)
-    timetracks = file.read()
+def save_timetrack(timetrack):
+    now_day = get_now_date()
+    tm = get_timetrack()
+    tm[now_day] = timetrack
+    file = open(TIME_TRACK_JSON, 'w')
+    file.write(json.dumps(tm)) 
     file.close()
-    timelines = timetracks.split('\n')
+
+def get_timetrack(date=None):
+    file = open(TIME_TRACK_JSON)
+    timetrack = json.loads(file.read())
+    file.close()
+
+    if date != None:
+        if date in timetrack:
+            return timetrack[date]
+        else:
+            return timetrack['2016-3-3']
+
+    return collections.OrderedDict(sorted(timetrack.items()))
+
+
+
+
+## SHOW TIMES
+def show_times():
+    timetrack = get_timetrack()
+
+    timeline = ''
+    message = ''
     total_hour = 0
     total_minute = 0
 
-    for timeline in timelines[-5:]:
-        timeline = timeline.split('#')
-        date = timeline[0]
+    for date in timetrack:
+        timeline = timetrack[date]['tracking']
+        message = timetrack[date]['message']
 
         if not len(timeline) > 1: 
             continue
@@ -38,16 +70,16 @@ def show_times():
         print '#'*30
         print ''
         print date+':\n'
-        for time in timeline[1:]:
+        for time in timeline:
             try:
                 times = time.split('|')
-                message = re.split('[{}]', times[1])[1]#.index('{')
+                #message = re.split('[{}]', times[1])[1]#.index('{')
                 #print re.split('[{}]', times[1])
                 #print times
-                times[1] = times[1].split('{')[0]
+                #times[1] = times[1].split('{')[0]
 
                 if times[1] == '':
-                    times[1] = datetime.now().strftime("%H:%M") 
+                    times[1] = get_now_time()
 
                 hour = int(times[1].split(':')[0]) - int(times[0].split(':')[0])
                 minute = int(times[1].split(':')[1]) - int(times[0].split(':')[1])
@@ -59,9 +91,9 @@ def show_times():
                 if minute < 10:
                     minute = "0"+str(minute)
 
-                if times[1] == datetime.now().strftime("%H:%M"):
+                if times[1] == get_now_time():
                     times[1] = 'now'
-                print '\t'+times[0]+"|"+times[1]+'\t'+str(hour)+":"+str(minute)+"\t"+message
+                print '\t'+times[0]+"|"+times[1]+'\t'+str(hour)+":"+str(minute)
             except:
                 pass
 
@@ -73,7 +105,7 @@ def show_times():
         print '-'*30
         if sum_minute < 10:
             sum_minute = "0"+str(sum_minute)
-        print 'Odpracovany cas:\t'+str(sum_hour)+":"+str(sum_minute)
+        print 'Odpracovany cas:\t'+str(sum_hour)+":"+str(sum_minute)+"\t"+message
         print '\n'
 
         total_hour += sum_hour
@@ -87,39 +119,29 @@ def show_times():
     print 'Total:\t\t\t'+str(total_hour)+":"+str(total_minute)
         
 
-def get_last_day():
-    file = open(TIME_TRACK_FILE)
-    timetracks = file.read()
-    timelines = timetracks.split('\n')
-    while '' in timelines: 
-        timelines.remove('')
-
-    file.close()
-    return timelines[-1].split('#')
-
 
 ### INPUT TIME
 def input_time(status, message=''):
-    file = open(TIME_TRACK_FILE, 'a')
+    now_day = get_now_date()
+    timetrack = get_timetrack(date=now_day)
+    tracking = timetrack['tracking']
+    timetrack['message'] += message
 
-    now_day = datetime.now().strftime("%Y-%m-%d")
-    now_time = datetime.now().strftime("%H:%M")
 
-    last_day = get_last_day()
+    now_time = get_now_time()
+
 
     if status == 'entry':
-        if last_day[0] == now_day:
-            file.write("#%s|" % (now_time,))
-        else:
-            file.write("\n%s#%s|" % (now_day, now_time))
+        tracking.append("%s|" % (now_time,))
+        print tracking
 
     if status == 'leave':
-        if last_day[0] == now_day:
-            file.write("%s{%s}" % (now_time, message))
-        else:
-            file.write("\n%s#|%s{%s}" % (now_day, now_time, message))
+        tracking[len(tracking)-1] += now_time
+        print tracking
 
-    file.close()
+
+    save_timetrack(timetrack)
+
     show_times()
 
 def set_status(status):
@@ -137,8 +159,8 @@ def get_status():
 if not os.path.exists(TIME_TRACK_FILE):
     now_day = datetime.now().strftime("%Y-%m-%d")
     now_time = datetime.now().strftime("%H:%M")
-    with open(TIME_TRACK_FILE, 'w') as timetrack_file:
-        timetrack_file.write("\n"+now_day+"#"+now_time+"|")
+#    with open(TIME_TRACK_FILE, 'w') as timetrack_file:
+#        timetrack_file.write("\n"+now_day+"#"+now_time+"|")
     set_status('0')
     show_times()
     
@@ -149,8 +171,11 @@ if len(sys.argv)>1:
             set_status('0')
     if sys.argv[1]=='out':
         message = ''
-        if sys.argv[2]=='-m':
-            message = sys.argv[3]
+        try:
+            if sys.argv[2]=='-m':
+                message = sys.argv[3]
+        except:
+            pass
         if get_status() == '0':
             input_time('leave', message)
             set_status('1')
@@ -160,7 +185,8 @@ if len(sys.argv)>1:
     if sys.argv[1]=='status':
         show_times()
     if sys.argv[1]=='src' or sys.argv[1]=='source':
-        os.system('cat '+TIME_TRACK_FILE)
+        print TIME_TRACK_JSON
+        os.system('cat '+TIME_TRACK_JSON)
 
 else:
     show_times()
